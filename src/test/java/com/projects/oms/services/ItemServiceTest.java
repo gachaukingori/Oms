3b8739b35d210100dc8e642f2e35acb38cc29436
shauuu <victor.kingori93@gmail.com>
2023-06-12 22:37:35 +0300

    started covering mocking

--- src/test/java/com/projects/oms/services/ItemServiceTest.java
+++ src/test/java/com/projects/oms/services/ItemServiceTest.java
@@ -1,97 +1,63 @@
 package com.projects.oms.services;
 
-import com.projects.oms.exceptions.NotFoundException;
-import com.projects.oms.models.Item;
+import com.projects.oms.dto.ItemDTOMapper;
 import com.projects.oms.repositories.ItemRepository;
-import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
-import java.util.Optional;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
 
-@DataJpaTest
 
 class ItemServiceTest {
-    @Autowired
-    private  ItemRepository itemRepository;
-
-
+   @Mock
+   private ItemRepository itemRepository;
+    @Mock
+    private ItemDTOMapper itemDTOMapper;
+    private ItemService underTest;
+    private AutoCloseable autoCloseable;
     @BeforeEach
     void setUp() {
+        autoCloseable = MockitoAnnotations.openMocks(this);
+        underTest = new ItemService(itemRepository, itemDTOMapper);
+
 
     }
-
     @AfterEach
-    void  tearDown(){
-        itemRepository.deleteAll();
+    void tearDown() throws Exception {
+        autoCloseable.close();
     }
 
     @Test
-    void shouldAddNewItem() {
-        // Given
-        Item item = new Item(
-                1,
-                54,
-                "bananas",
-                60.0,
-                7,
-                80) ;
-
-        itemRepository.save(item);
-        // when
-        Optional<Item> exists = itemRepository.findByItemNumber(54);
-        String itemDesc = exists.get().getItemDesc();
-
-        //Then
-        assertThat(itemDesc).isEqualTo(item.getItemDesc());
-
+    @Disabled
+    void addNewItem() {
 
     }
 
     @Test
-    void shouldCheckItemDoesNotExists(){
-        //given
-        Item item = new Item(
-                1,
-                54,
-                "bananas",
-                60.0,
-                7,
-                80) ;
-        // when
-        Optional<Item> exists = itemRepository.findByItemNumber(54);
-        boolean itemNumber = exists.isPresent();
 
-        //Then
-        assertThat(itemNumber).isEqualTo(false);
+    void getAllItems() {
+        // given
+        underTest.getAllItems();
+
+        //then
+        verify(itemRepository).findAll();
     }
 
+    @Test
+    @Disabled
+    void findItem() {
+    }
+
+    @Test
+    @Disabled
+    void deleteItem() {
+    }
 
     @Test
     void updateItemQuantity() {
-        // given
-         Item item = new Item(
-                1,
-                54,
-                "bananas",
-                60.0,
-                70,
-                80) ;
-         itemRepository.save(item);
-
-         itemRepository.updateItemQuantity(100, 54);
-         // when
-        Optional<Item> item1 = itemRepository.findByItemNumber(54);
-        int itemQuantity= item1.orElseThrow(()->new NotFoundException("item not found")).getItemQuantity();
-        // then
-        assertThat(itemQuantity).isEqualTo(100);
-
     }
 }
