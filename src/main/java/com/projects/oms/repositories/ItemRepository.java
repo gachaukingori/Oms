48bf5ca011ebad7716e47c4b47039b0d9861b317
shauuu <victor.kingori93@gmail.com>
2023-08-04 23:24:25 +0300

    created more jpa test

--- src/main/java/com/projects/oms/repositories/ItemRepository.java
+++ src/main/java/com/projects/oms/repositories/ItemRepository.java
@@ -2,12 +2,14 @@ package com.projects.oms.repositories;
 
 import com.projects.oms.models.Item;
 import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -18,4 +20,9 @@ public interface ItemRepository extends JpaRepository<Item, Integer> {
     @Modifying(clearAutomatically = true)
     @Query("UPDATE Item SET itemQuantity = :itemQuantity WHERE itemNumber = :itemNumber ")
     int updateItemQuantity(@Param("itemQuantity") int itemQuantity, @Param("itemNumber") int itemNumber);
+    @Query(value="SELECT i.id, i.itemNumber, i.itemDesc, i.purchasePrice, i.itemQuantity, i.salePrice FROM Items i", nativeQuery = true)
+    List<Item> getAllItemsWithPagination(Pageable pageable);
+
+    @Query(value = "SELECT AVG(i.purchasePrice) as avgPurchasePrice FROM Items i",nativeQuery = true)
+    Double getAveragePurchasePrice();
 }
--- src/test/java/com/projects/oms/repositories/ItemRepositoryTest.java
+++ src/test/java/com/projects/oms/repositories/ItemRepositoryTest.java
@@ -1,12 +1,19 @@
 package com.projects.oms.repositories;
 
+import com.projects.oms.Controllers.CustomerController;
 import com.projects.oms.models.Item;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
@@ -14,6 +21,8 @@ import static org.junit.jupiter.api.Assertions.*;
 @DataJpaTest
 @RequiredArgsConstructor
 class ItemRepositoryTest {
+    Logger logger =
+            LoggerFactory.getLogger(ItemRepositoryTest.class);
     @Autowired
     private  ItemRepository underTest;
     @BeforeEach
@@ -41,7 +50,7 @@ class ItemRepositoryTest {
         //Then
         assertThat(itemNumber).isEqualTo(89);
     }
-    
+
 
     @Test
     void updateItemQuantity() {
@@ -58,12 +67,128 @@ class ItemRepositoryTest {
         // when
         underTest.save(item);
         underTest.updateItemQuantity(800,89);
-        
-        Optional<Item>  expectedItem = underTest.findById(item.getId());
+
+        Optional<Item>  expectedItem = underTest.findByItemNumber(item.getItemNumber());
         int itemQuantity = expectedItem.orElseThrow().getItemQuantity();
 
         //Then
         int expected = 800;
         assertThat(itemQuantity).isEqualTo(expected);
+    }
+
+    @Test
+    void testOrderBy(){
+        //given
+        Item item = new Item(
+                1,
+                89,
+                "Pineapple",
+                70,
+                8,
+                40
+        );
+        Item item2 = new Item(
+                2,
+                90,
+                "mango",
+                50,
+                60,
+                70
+        );
+        Item item3= new Item(
+                3,
+                91,
+                "banana",
+                10,
+                50,
+                30
+        );
+        //when
+        underTest.save(item2);
+        underTest.save(item3);
+        underTest.save(item);
+
+
+        List<Item> orderedByName = underTest.findAll(Sort.by(Sort.Direction.DESC,"itemQuantity"));
+
+        //then
+        assertThat(orderedByName.get(0).getItemDesc()).isEqualTo("mango");
+    }
+    @Test
+    void testAveragePrice(){
+        //given
+        Item item = new Item(
+                1,
+                89,
+                "Pineapple",
+                90,
+                8,
+                40
+        );
+        Item item2 = new Item(
+                2,
+                90,
+                "mango",
+                50,
+                60,
+                70
+        );
+        Item item3= new Item(
+                3,
+                91,
+                "banana",
+                10,
+                50,
+                30
+        );
+
+        //when
+        underTest.save(item2);
+        underTest.save(item3);
+        underTest.save(item);
+
+        Double avgPrice = underTest.getAveragePurchasePrice();
+        //then
+        assertThat(avgPrice).isEqualTo(50);
+    }
+    @Test
+    void testPagination(){
+        //given
+        Pageable pageable = PageRequest.of(0,3);
+        Item item = new Item(
+                1,
+                89,
+                "Pineapple",
+                70,
+                8,
+                40
+        );
+        Item item2 = new Item(
+                2,
+                90,
+                "mango",
+                50,
+                60,
+                70
+        );
+        Item item3= new Item(
+                3,
+                91,
+                "banana",
+                10,
+                50,
+                30
+        );
+        //when
+        underTest.save(item2);
+        underTest.save(item3);
+        underTest.save(item);
+
+        List<Item> theFirst3Items = underTest.getAllItemsWithPagination(pageable);
+
+        //then
+        assertThat(theFirst3Items.get(1).getItemDesc()).isEqualTo("banana");
+
+
     }
 }
